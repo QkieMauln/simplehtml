75284a8eead3c61186042a28a8f1989c4a9d34ee
qky <qkie0915@gmail.com>
2020-03-01 04:59:09 +0700

    Huge changes

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@
 // --> Unless you know what are you doing <--
 // <!> Dont delete dis <!>
 //Blank Code. Maybe Later
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scrollcontainer").style.width = scrolled + "%";
+}
+
+function toggle(e){
+	var val = document.getElementById(e);
+	if(val.style.display == "none") return val.style.display = "block";
+	val.style.display = "none";
+}
